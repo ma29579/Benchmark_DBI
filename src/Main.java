d9ec7f380845f0764c51de57cb01c00389b0f26c
Enthöfer Joshua <j.enthoefer@laudert.de>
2019-11-27 08:58:07 +0100

    1. Versuch die 100000.

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,8 @@ public class Main {
         try {
 
 //            DBHelper reader = new DBHelper("jdbc:postgresql://localhost:5432/DBI", "postgres", "postgres");
-            DBHelper reader = new DBHelper("jdbc:postgresql://localhost:5432/DBI", "postgres", "dbidbi");
-//            DBHelper reader = new DBHelper("jdbc:postgresql://localhost:5433/postgres", "jen", "");
+//            DBHelper reader = new DBHelper("jdbc:postgresql://localhost:5432/DBI", "postgres", "dbidbi");
+            DBHelper reader = new DBHelper("jdbc:postgresql://localhost:5433/postgres", "jen", "");
             reader.executeUpdate(test);
             reader.insertData(n);
 
