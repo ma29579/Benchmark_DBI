8b473ed7edd9aeeb1bffcc80f94bc31d8e86a13d
ma29579 <maximilian.thomas@studmail.w-hs.de>
2019-12-03 11:23:21 +0100

    1.Version Transaktions-Benchmarks JDBC-Connection Serialisiert Phasen implementiert

--- src/DBReader.java
+++ src/DBReader.java
@@ -1,2 +1,80 @@
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
 public class DBReader {
+
+    Connection conn;
+    PreparedStatement stmt;
+    ResultSet results;
+
+
+    //Konstruktor
+    DBReader(String databaseURL, String username, String password) throws SQLException{
+        conn = getConnection(databaseURL,username,password);
+        conn.setAutoCommit(false);
+        conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+    }
+
+    public int kontostandTransaktion(int accid) throws SQLException {
+
+        stmt = conn.prepareStatement("SELECT balance FROM tps100.accounts WHERE accid = " + accid);
+        results = stmt.executeQuery();
+        results.next();
+
+        return results.getInt(1);
+    }
+
+    public int einzahlungsTransaktion(int accid,int tellerid, int branchid, int delta) throws SQLException {
+
+        int balance = 0;
+
+        String updateBranches = "UPDATE tps100.branches SET balance = balance + " + delta  + "WHERE branchid = " + branchid + ";";
+        String updateTellers = "UPDATE tps100.tellers SET balance = balance + " + delta  + "WHERE tellerid = " + tellerid + ";";
+        String updateAccounts = "UPDATE tps100.accounts SET balance = balance + " + delta  + "WHERE accid = " + accid + ";";
+
+        String selectBalance = "SELECT balance FROM tps100.accounts WHERE accid = " + accid;
+
+        String insertIntoHistory = "INSERT INTO tps100.history(accid,tellerid,delta,branchid,accbalance,cmmnt) " +
+                                    "VALUES (" + accid + "," + tellerid + "," + delta + "," + branchid + ", ?" +
+                                    ",'012345678901234567890123456789');";
+
+        stmt = conn.prepareStatement(updateBranches + updateTellers + updateAccounts);
+        stmt.executeUpdate();
+        stmt.close();
+
+        stmt = conn.prepareStatement(selectBalance);
+        results = stmt.executeQuery();
+        results.next();
+        balance = results.getInt(1);
+        stmt.close();
+
+        stmt = conn.prepareStatement(insertIntoHistory);
+        stmt.setInt(1,balance);
+        stmt.executeUpdate();
+        stmt.close();
+
+        conn.commit();
+
+        return balance;
+    }
+
+    public void clearHistory() throws SQLException{
+
+        stmt = conn.prepareStatement("DELETE FROM tps100.history");
+        stmt.executeUpdate();
+
+        conn.commit();
+    }
+
+    public int analyseTransaktion(int delta) throws SQLException {
+
+        stmt = conn.prepareStatement("SELECT COUNT(*) FROM tps100.history WHERE delta = " + delta);
+        results = stmt.executeQuery();
+
+        results.next();
+
+        return results.getInt(1);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -11,9 +12,9 @@ public class Main {
 
         int n;
 
-        System.out.println("Eingabe TPS-Anzahl:");
+        //System.out.println("Eingabe TPS-Anzahl:");
 
-        n = input.nextInt();
+        //n = input.nextInt();
 
         String databaseURL = "";
         String username = "";
@@ -40,15 +41,72 @@ public class Main {
 
         try {
             // Instanziierung des DBWriters
-            DBWriter writer = new DBWriter(databaseURL, username, password);
+            //DBWriter writer = new DBWriter(databaseURL, username, password);
+
+            DBReader reader = new DBReader(databaseURL,username,password);
+
+            run(reader);
 
             // Initialisierung der Benchmark-Datenbank
-            writer.createBenchmarkDatabase(n);
-            writer.insertData(n);
-            writer.closeConnection();
+            //writer.createBenchmarkDatabase(n);
+            //writer.insertData(n);
+            //writer.closeConnection();
 
         } catch (SQLException e) {
             System.out.println("Fehler bei der Datenbankverbindung!");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void run(DBReader reader) throws SQLException {
+        reader.clearHistory();
+
+        long startTime = System.currentTimeMillis();
+        int count = 0;
+
+        // Einschwingphase
+        while(System.currentTimeMillis() - startTime < 240000){
+            execute(reader);
+        }
+
+        // Messphase
+        while(System.currentTimeMillis() - startTime < 540000){
+            execute(reader);
+            count++;
+        }
+
+        System.out.println("count = " + count);
+
+        // Ausschwingphase
+        while (System.currentTimeMillis() - startTime < 600000){
+            execute(reader);
+        }
+
+    }
+
+    public static void execute(DBReader reader) throws SQLException {
+
+        int chosenMethod = ThreadLocalRandom.current().nextInt(0,100);
+
+
+        if(chosenMethod < 35){
+            reader.kontostandTransaktion(ThreadLocalRandom.current().nextInt(1,10000000));
+        }
+        else if(chosenMethod < 85){
+            reader.einzahlungsTransaktion(
+                    ThreadLocalRandom.current().nextInt(1,10000000),    // AccountID
+                    ThreadLocalRandom.current().nextInt(1,1000),        // TellerID
+                    ThreadLocalRandom.current().nextInt(1,100),         // BranchID
+                    ThreadLocalRandom.current().nextInt(5,501));        // Delta
+        }
+        else{
+            reader.analyseTransaktion(ThreadLocalRandom.current().nextInt(5,501));
+        }
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
